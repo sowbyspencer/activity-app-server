9ce32a024d91cc4d58bc3aa7a56ea4ea19c02153
sowbyspencer <sowbyspencer@gmail.com>
2025-03-27 14:28:32 -0600

    db.ts edits to check env vars

--- db.ts
+++ db.ts
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "postgres",
-    password: "postgres",
-    port: 5432, // Default PostgreSQL port
+    connectionString: process.env.DATABASE_URL || null,
+    user: process.env.DB_USER || "postgres",
+    host: process.env.DB_HOST || "localhost",
+    database: process.env.DB_NAME || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432, // Default PostgreSQL port
 });
 
 module.exports = pool;
