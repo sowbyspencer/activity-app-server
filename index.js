3978df57db2b3ab8c067684ca820be137e7584e2
sowbyspencer <sowbyspencer@gmail.com>
2025-04-09 10:31:26 -0600

    Cors problem

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:8081", // Allow requests from your frontend's origin
+    methods: "GET,POST,PUT,DELETE", // Specify allowed HTTP methods
+    credentials: true, // Allow cookies if needed
+  })
+);
 app.use(express.json());
 app.use("/public", express.static("public"));
 
