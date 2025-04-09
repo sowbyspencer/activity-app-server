25deca37b42975f4b479a5b4680cf7c8c908285d
sowbyspencer <sowbyspencer@gmail.com>
2025-04-09 10:49:21 -0600

    Retrying the cors issue fix

--- index.js
+++ index.js
@@ -15,7 +15,16 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:8081",
+      "http://activity-app-server.onrender.com",
+    ], // Allow multiple origins
+    methods: "GET,POST,PUT,DELETE", // Specify allowed HTTP methods
+    credentials: true, // Allow cookies if needed
+  })
+);
 app.use(express.json());
 app.use("/public", express.static("public"));
 
@@ -31,7 +40,7 @@ app.listen(PORT, () => {
   console.log(
     `🔌 Connected to ${
       !process.env.DB_ENV || process.env.DB_ENV === "hosted"
-        ? "Ptroduction DB"
+        ? "Production DB"
         : "Local DB"
     }`
   );
